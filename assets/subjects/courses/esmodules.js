59f9f6d7ca8e58806eb03a7b67a407d2ba3c8f15
FBerthelot <florent.berthelot35@gmail.com>
2021-09-10 11:35:52 +0200

    Rebrandring courses

--- assets/subjects/courses/esmodules.js
+++ assets/subjects/courses/esmodules.js
@@ -1,10 +1,10 @@
 module.exports = {
   name: 'Les modules en JavaScript',
   description: `
-    L'histoire de la séparation en plusieurs fichiers des applications JavaScript a été et est encore assez mouvementé.
+    L'histoire de la séparation en plusieurs fichiers des applications JavaScript a été assez mouvementé.
 
     <br></br>
     Voyons ensemble les enjeux de Node.js, des navigateurs et de Ecma International.
   `,
-  url: '/slides/es-modules.html',
+  url: 'https://slides.com/florentberthelot/les-modules-javascript/',
 }
---
+++ assets/subjects/courses/javascriptCraftmanship.js
@@ -0,0 +1,9 @@
+module.exports = {
+  name: 'JavaScript Craftmanship',
+  description: `
+    Les tests sont souvent considéré comme innutiles et complexes. Le TDD ? Une utopie !
+    <br/><br/>
+    Démystifions les tests dans l'écosystème JavaScript pour voir ensemble ce qui vous corresponds au mieux.
+  `,
+  url: 'https://slides.com/florentberthelot/javascript-craftmanship/',
+}
