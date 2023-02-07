13ed6ce1e8645df0390c93f468be7f517caec317
FBerthelot <FBerthelot@users.noreply.github.com>
2023-02-07 14:03:02 +0000

    Deploying to gh-pages from @ FBerthelot/berthelot.io@4e258819afb834f44cc1f2b05270ec8c0f0bf39d 🚀

---
+++ _nuxt/static/1675778576/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b,c){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1675778576",layout:"default",error:b,serverRendered:true,routePath:c,config:{SHEETDB_URL:a,SLACK_URL:a,MAPS_URL:a,_app:{basePath:c,assetsPath:"\u002F_nuxt\u002F",cdnURL:b}}}}(void 0,null,"\u002F"));
